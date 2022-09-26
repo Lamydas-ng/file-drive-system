b786609fe959ea2ddf17c4591b73849931ab1552
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-26 17:01:07 +0100

    adjusted  client services scripts

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.VUE_APP_BASE_URL;
+//const BASE_URL = import.meta.env.VUE_APP_BASE_URL;
+const BASE_URL = "http://localhost:3030";
 
 const http = axios.create({
     baseURL: BASE_URL
