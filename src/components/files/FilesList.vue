8f48c0adbff4c0e2f03623cdaf638d7aaa62906a
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-27 15:03:05 +0100

    Added custom file icons to  FileItem component

--- src/components/files/FilesList.vue
+++ src/components/files/FilesList.vue
@@ -1,25 +1,13 @@
 <template>
         <div class="row">
-            <div class="col-md-3" v-for="file in files" :key="`file-${file.id}`">
-            <div class="card mb-4">
-                <img class="file-thumb" :src="file.url" v-if="file.url" />
-                <div class="card-body text-center py-5" v-else>
-                    <icon-type-common height="4em" width="4em"  />
-                </div>
-                <div class="card-footer">
-                <div class="d-flex align-items-center">
-                    <icon-type-common />
-                    <span class="file-name">{{ file.name}}</span>
-                </div>
-                </div>
-            </div>
-            </div>
-    </div>
+           <file-item v-for="file in files" :file="file" :key="`file-${file.id}`"></file-item>
+         </div>
 </template>
 
 <script>
+    import  FileItem from './FileItem.vue'
 export default {
-    components: {},
+    components: { FileItem },
     props:{
        files: {
         type: Array,
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -8,24 +8,7 @@
         <icon-arrow-up />
       </button>
     </div>
-    <div class="row">
-
-
-      <div class="col-md-3" v-for="file in files" :key="`file-${file.id}`">
-        <div class="card mb-4">
-          <img class="file-thumb" :src="file.url" v-if="file.url" />
-            <div class="card-body text-center py-5" v-else>
-              <icon-type-common height="4em" width="4em"  />
-            </div>
-          <div class="card-footer">
-            <div class="d-flex align-items-center">
-              <icon-type-common />
-              <span class="file-name">{{ file.name}}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <files-list :files="files"></files-list>
   </div>
 </template>
 
@@ -33,10 +16,12 @@
 
 import ActionBar from "../components/ActionBar.vue";
 import fileApi from "../api/files";
+import FilesList from "../components/files/FilesList.vue";
+
 
 
 export default {
-  components: { ActionBar },
+  components: { ActionBar, FilesList },
   mounted() {
     this.fetchFiles();
   },
