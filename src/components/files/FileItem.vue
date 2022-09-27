0cf43f90f78129b73eb248f773ba9da8e418a0ae
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-27 12:49:29 +0100

    FileItem and FilesLits Components were created

---
+++ src/components/files/FileItem.vue
@@ -0,0 +1,26 @@
+<template>
+     <div class="col-md-3" >
+            <div class="card mb-4">
+                <img class="file-thumb" :src="file.url" v-if="file.url" />
+                <div class="card-body text-center py-5" v-else>
+                    <icon-type-common height="4em" width="4em"  />
+                </div>
+                <div class="card-footer">
+                <div class="d-flex align-items-center">
+                    <icon-type-common />
+                    <span class="file-name">{{ file.name}}</span>
+                </div>
+                </div>
+            </div>
+            </div>
+</template>
+<script>
+export default {
+    props:{
+        file:{
+            type: Object,
+            require: true
+        }
+    }
+}
+</script>
---
+++ src/components/files/FilesList.vue
@@ -0,0 +1,30 @@
+<template>
+        <div class="row">
+            <div class="col-md-3" v-for="file in files" :key="`file-${file.id}`">
+            <div class="card mb-4">
+                <img class="file-thumb" :src="file.url" v-if="file.url" />
+                <div class="card-body text-center py-5" v-else>
+                    <icon-type-common height="4em" width="4em"  />
+                </div>
+                <div class="card-footer">
+                <div class="d-flex align-items-center">
+                    <icon-type-common />
+                    <span class="file-name">{{ file.name}}</span>
+                </div>
+                </div>
+            </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props:{
+       files: {
+        type: Array,
+        required:true
+       } 
+    }
+}
+</script>
