b1061e9d9ef02983bfea22ebcb5cafcabcf4d616
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-09-29 15:49:06 +0100

    Added search form functionality using Vue teleport techniques

---
+++ src/components/SortToggler.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="rounded-button" @click ="toggle">
+        <icon-arrow-up    v-if=" sort.order === 'asc' "/>
+        <icon-arrow-down v-else/>
+
+      </button>
+</template>
+
+<script>
+    import { emit, reactive } from 'vue';
+
+export default {
+    setup(props,{ emit }){
+
+        const sort = reactive({
+            order: 'asc',
+            column: 'name' 
+        });
+
+        const toggle = () => {
+
+            sort.order = sort.order === 'asc'? 'desc':'asc';
+            emit('sort-change', sort);
+        }
+
+        return { sort , toggle};
+
+    },
+    emits:['sort-change']
+}
+</script>
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -4,25 +4,23 @@
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Files</h6>
-      <button class="rounded-button">
-        <icon-arrow-up />
-      </button>
+      <sort-toggler @sort-change="handleSortChange($event)"/>
     </div>
     <files-list :files="files"></files-list>
   </div>
 </template>
 
 <script>
-
+import SortToggler from '../components/SortToggler.vue';
 import ActionBar from "../components/ActionBar.vue";
 import fileApi from "../api/files";
 import FilesList from "../components/files/FilesList.vue";
-import {ref, onMounted } from 'vue';
+import {ref, onMounted, reactive, watchEffect } from 'vue';
 
 
-const fetchFiles = async () => {
+const fetchFiles = async (query) => {
       try {
-        const { data } = await fileApi.index();
+        const { data } = await fileApi.index(query);
         return data;
       } catch (error) {
         console.error(error);
@@ -30,15 +28,26 @@ const fetchFiles = async () => {
     };
 
 export default {
-  components: { ActionBar, FilesList },
+  components: { ActionBar, FilesList, SortToggler },
 
   setup(){
     const files =  ref([]);
-    onMounted( async() => files.value = await fetchFiles() );
+    const query = reactive({
+      _sort : 'name',
+      _order : 'asc'
+
+    });
+
+    const handleSortChange = (payload) => {
+          query._sort = payload.column;
+          query._order = payload.order;
+    };
+
+    watchEffect( async() => files.value = await fetchFiles(query) );
 
 
 
-    return { files };
+    return { files, handleSortChange };
   }
 
   
