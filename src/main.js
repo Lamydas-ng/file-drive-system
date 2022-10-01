7631810477d832231c5200ae11df8f9c1b10a77c
lamydas-ng <lamydas.ng@gmail.com>
2022-10-01 10:56:28 +0100

    Added toast notification

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import toast from "./components/toast/Toast.vue";
 import icons from "./components/icons/all";
 
 const app = createApp(App);
@@ -10,4 +11,5 @@ for (const [key, value] of Object.entries(icons)) {
   app.component(key, value);
 }
 
+app.component("app-toast", toast);
 app.mount("#app");
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-3">
-    <ActionBar :selected-count="selectedItems.length" />
+    <ActionBar :selected-count="selectedItems.length" @remove="handleRemove" />
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Files {{ selectedItems }}</h6>
@@ -12,6 +12,9 @@
 
     <files-list :files="files" @select-change="handleSelectChange($event)"></files-list>
 
+    <app-toast :show="toast.show" :message="toast.message" position="bottom-left" type="success" @hide="toast.show=false"></app-toast>
+
+
   </div>
 </template>
 
@@ -19,22 +22,42 @@
 import SortToggler from '../components/SortToggler.vue';
 import ActionBar from "../components/ActionBar.vue";
 import SearchForm from '../components/SearchForm.vue';
-import fileApi from "../api/files";
+import filesApi from "../api/files";
 import FilesList from "../components/files/FilesList.vue";
 import {ref,  reactive, watchEffect, toRef } from 'vue';
+import { remove } from '@vue/shared';
+import Toast from '../components/toast/Toast.vue';
 
 
 const fetchFiles = async (query) => {
       try {
-        const { data } = await fileApi.index(query);
+        const { data } = await filesApi.index(query);
         return data;
       } catch (error) {
         console.error(error);
       }
-    };
+};
+
+
+
+const removeItem = async (item, files) => {
+
+      try {
+          const response = await filesApi.delete(item.id);
+          if (response.status === 200 || response.status === 204) {
+            const index = files.value.findIndex(file => file.id === item.id);
+            files.value.splice(index, 1);
+
+          }
+      } catch (error) {
+          console.error(error);
+      }
+
+};
+
 
 export default {
-  components: { ActionBar, FilesList, SortToggler,SearchForm },
+  components: { ActionBar, FilesList, SortToggler, SearchForm, Toast },
 
   setup(){
 
@@ -47,6 +70,10 @@ export default {
 
     });
 
+      const toast = reactive({
+        show: false,
+        message: ""
+      });
     const handleSortChange = (payload) => {
           query._sort = payload.column;
           query._order = payload.order;
@@ -56,10 +83,19 @@ export default {
       selectedItems.value = Array.from(items);
     }
 
+    const handleRemove = () => {
+      if(confirm("Are you sure?"))
+      selectedItems.value.forEach((item) => removeItem(item, files));
+      selectedItems.value.splice(0);
+      toast.show = true;
+      toast.message = "Selected Item(s)successfully deleted";
+    }
+
     watchEffect( async() => files.value = await fetchFiles(query) );
 
 
-    return { files, handleSortChange, q: toRef(query, "q") , handleSelectChange, selectedItems };
+
+    return { files, handleSortChange, q: toRef(query, "q") , handleSelectChange, selectedItems,handleRemove, toast  };
   }
 
 
