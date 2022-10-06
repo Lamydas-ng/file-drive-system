37c0c489a310f0a2f7fdbd38225607099f885dce
lamydas-ng <lamydas.ng@gmail.com>
2022-10-06 22:00:47 +0100

    Added functionality to  modal form component

---
+++ src/components/files/FileRenameForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <form >
+        <Input type="text" class="form-control"> </Input>
+        <div class="d-flex dflex-row-reverse mt-3">
+            <button class="btn btn-primary">
+                Ok
+            </button>
+            <button class="btn btn-outline-secondary me-2">
+                Cancel
+            </button>
+        </div>
+    </form>
+</template>
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-3">
-    <ActionBar :selected-count="selectedItems.length" @remove="handleRemove" />
+    <ActionBar :selected-count="selectedItems.length" @remove="handleRemove" @rename="showModal=true" />
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Files {{ selectedItems }}</h6>
@@ -14,7 +14,13 @@
 
     <app-toast :show="toast.show" :message="toast.message" position="bottom-left" type="success" @hide="toast.show=false"></app-toast>
 
-
+    <app-modal
+      title="Rename"
+       :show="showModal && selectedItems.length === 1"
+       @hide="showModal=false"
+       >
+       <file-rename-form />
+</app-modal>
   </div>
 </template>
 
@@ -24,6 +30,7 @@ import ActionBar from "../components/ActionBar.vue";
 import SearchForm from '../components/SearchForm.vue';
 import filesApi from "../api/files";
 import FilesList from "../components/files/FilesList.vue";
+import FileRenameForm from "../components/files/FileRenameForm.vue";
 import {ref,  reactive, watchEffect, toRef } from 'vue';
 
 import Toast from '../components/toast/Toast.vue';
@@ -57,12 +64,13 @@ const removeItem = async (item, files) => {
 
 
 export default {
-  components: { ActionBar, FilesList, SortToggler, SearchForm, Toast },
+  components: { ActionBar, FilesList, SortToggler, SearchForm, Toast, FileRenameForm },
 
   setup(){
 
     const files = ref([]);
     const selectedItems = ref([]);
+    const showModal = ref(false);
     const query = reactive({
       _sort : 'name',
       _order : 'asc',
@@ -95,7 +103,17 @@ export default {
 
 
 
-    return { files, handleSortChange, q: toRef(query, "q") , handleSelectChange, selectedItems,handleRemove, toast  };
+    return {
+      files,
+      handleSortChange,
+      q: toRef(query, "q"),
+      handleSelectChange,
+      selectedItems,
+      handleRemove,
+      toast,
+      showModal,
+      FileRenameForm
+    };
   }
 
 
