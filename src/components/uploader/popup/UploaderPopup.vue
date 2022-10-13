53fb6adc3cfbe3864127b84d4fde223bd64d66c5
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-10-13 04:44:34 +0100

    Added upload popup component

---
+++ src/components/uploader/popup/UploaderPopup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card shadow uploader-popup">
+     <div class="card header bg-dark py-3">
+       <div class="d-dflex justify-content-between align-items-center">
+         <span class="text-light"> Uploading </span>
+            <div class="popup-controls">
+                <button class="rounded-button me-2">
+                    <icon-chevron-down />
+                </button>
+                <button class="rounded-button">
+                    <icon-times></icon-times>
+                </button>
+            </div>
+       
+     </div>
+
+     </div>
+    </div>
+ </template>
