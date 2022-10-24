7c81776a043a728ca28eb64b8b67ca32299db67a
RUFAI Ahmed <lamydas.ng@gmail.com>
2022-10-24 16:37:49 +0100

    Fixe json server storage link issue

--- src/components/uploader/item/UploadItem.vue
+++ src/components/uploader/item/UploadItem.vue
@@ -4,27 +4,69 @@
                 <component :is="iconFileType"></component>
                 <span> {{ item.file.name}}</span>
                 </p>
-                <div class="upload-controls">x</div>
+                <div class="upload-controls">{{ `${uploadItem.state} : ${uploadItem.progress}`}}</div>
 
             </li>
 
 </template>
 
 <script>
+import { reactive, onMounted } from 'vue'
  import {useIconFileType} from '../../../composable/icon-file-type'
+ import states from '../states'
+ import filesApi from '../../../api/files'
 export default {
 
-    props: {
+ 
+  
+  props: {
     item: {
       type: Object,
         required: true
 
-    }
+    },
 
     },
+
     setup(props){
 
+      const uploadItem = reactive(props.item) ;
+      
+      const createFormData = (file)=>{
+        const formData = new FormData();
+        formData.append("file",file);
+        return formData;
+
+      }
+      
+      const startUpload = async (upload) =>
+      {
+       
+       try {
+
+         upload.state = states.UPLOADING;  
+//  console.log(upload);
+           const {data} = await filesApi.create(createFormData(upload.file),{
+ 
+           onUploadProgress: (e)=>{
+             if (e.lengthComputable){
+               upload.progress = Math.round((e.loaded/e.toatl)*100)
+             }
+           }
+           
+         })
+        upload.state = states.COMPLETE;
+        upload.response = data; 
+      } catch (error) {
+        upload.state = states.FAILED;
+        upload.response = 0; 
+       }
+
+      }
+      onMounted( startUpload(uploadItem));
+
       return {
+        uploadItem,
         iconFileType: useIconFileType(props.item.file.type)
       }
 
